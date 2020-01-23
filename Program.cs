65d2275a4e4a1f2555cda91b8e4219048c507048
jensap1979 <60000152+jensap1979@users.noreply.github.com>
2020-01-23 20:36:53 +0200

    SIX

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 
-namespace exercise_05
+namespace exercise_06
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            string name = "Ada LoveLace";
-            string greeting = "Hello ";
-            Console.WriteLine(greeting + name + "!");
+            Console.WriteLine("Give good input!");
+             string message = Console.ReadLine();
+              Console.WriteLine("No need to stress" + message);
             
         }
     }
