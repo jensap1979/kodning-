2c3c201c92886e94e74f2d88f36333874c7568bb
jensap1979 <60000152+jensap1979@users.noreply.github.com>
2020-01-23 20:30:37 +0200

    TWO

--- Program.cs
+++ Program.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 
-namespace exercise_01
+namespace exercise_02
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            
-           Console.WriteLine("Hello World!");
-           
-    
-           
-
-
-            
-            
-        
+            Console.WriteLine("Once upon a time");
+            Console.WriteLine("I was falling in love");
+            Console.WriteLine("Now I'm only falling apart");
         }
     }
 }
