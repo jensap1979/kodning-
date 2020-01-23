f56fc2576a3dcbb024a796366fbd213582e2b08c
jensap1979 <60000152+jensap1979@users.noreply.github.com>
2020-01-23 20:40:06 +0200

    TEN

--- Program.cs
+++ Program.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 
-namespace exercise_09
+namespace exercise_10
 {
     class Program
     {
+    
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, how are you?");
-            string input = Console.ReadLine();
-            Console.WriteLine("That's interesting, tell me more");
-            string input_a = Console.ReadLine();
-            Console.WriteLine("Thank you for sharing!");
-            
-        }    
-         
-        
-            
-           
+            Console.WriteLine("I will tell a story, but I need some information.");
+            Console.WriteLine("Give a name for main character:");
+            string characterName = Console.ReadLine();
+            Console.WriteLine("Give the character a profession:");
+            string characterProfession = Console.ReadLine();
+            Console.WriteLine("Here is the story:" + "\n" +
+            "Once upon a time there was a " +  characterProfession  + " called " +  characterName +"\n"+
+            "On her way to work, " + characterName  + " often pondered what being " +  characterProfession + " meant to them."+"\n"+
+            "When you work as a " +  characterProfession + " you meet interesting people."+"\n"+
+            characterName  + " enjoys their work as " +  characterProfession + ", The end.");
+        }
 
     }
 }
